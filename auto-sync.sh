d880d35c76f1606799901d8cf8f05c57d351f74e
sli <sli@mail.bnu.edu.cn>
2020-08-10 03:10:09 +0800

    [add] sync

--- auto-sync.sh
+++ auto-sync.sh
@@ -3,12 +3,17 @@ while true
 do
     curl -s localhost:4040/http/in | grep -oP 'tcp://server.natappfree.cc:(\d*)' > proxy
     new=$(git status | grep proxy | wc -l)
-    echo $new
     if [ $new = 1 ]
     then
         git add proxy
-        git commit -m "new proxy"
+        git commit -m "new proxy - $(date '+%Y-%m-%d %H:%M:%S')"
     fi
-    git push origin master
-    sleep 5
+    push=$(git status | grep "\"git push\"" | wc -l)
+    if [ $new = 1 ]
+    then
+        git push origin master
+    else
+        echo nothing to do
+    fi
+    sleep 3600
 done
