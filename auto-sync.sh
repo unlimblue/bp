c3b4df9bcaf5c2107dd6819fbed5827245a377c3
sli <sli@mail.bnu.edu.cn>
2020-08-10 03:12:52 +0800

    [update]

--- auto-sync.sh
+++ auto-sync.sh
@@ -8,8 +8,8 @@ do
         git add proxy
         git commit -m "new proxy - $(date '+%Y-%m-%d %H:%M:%S')"
     fi
-    push=$(git status | grep "\"git push\"" | wc -l)
-    if [ $new = 1 ]
+    push=$(git log --decorate --oneline | head -n 1 | grep "origin/master" | wc -l)
+    if [ $new = 0 ]
     then
         git push origin master
     else
